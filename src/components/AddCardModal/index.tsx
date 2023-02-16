5e68396bf7ab491b89922791c81af40a9a4dc857
thiagocontelli <thiagocontellid@gmail.com>
2023-02-16 10:15:59 -0300

    feat: create AddCardModal component

---
+++ src/components/AddCardModal/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent, TextField
+} from '@mui/material';
+import { useState } from 'react';
+
+interface AddCardModalProps {
+  isOpen: boolean;
+  handleClose: () => void;
+  handleSubmit: (inputValue: string) => void;
+}
+
+export function AddCardModal({
+  handleClose,
+  isOpen,
+  handleSubmit
+}: AddCardModalProps) {
+  const [inputValue, setInputValue] = useState('')
+  
+  return (
+    <Dialog maxWidth="xs" open={isOpen} onClose={handleClose}>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          label="Card title"
+          fullWidth
+          variant="filled"
+          onChange={e => setInputValue(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button variant="contained" onClick={() => handleSubmit(inputValue)}>
+          Add
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
