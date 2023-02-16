139d382192841419663ae27b6c355dc6d9e5a6c1
thiagocontelli <thiagocontellid@gmail.com>
2023-02-16 09:10:29 -0300

    feat: add context provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { KanbanContextProvider } from "./hooks/useKanban";
 import { darkTheme } from "./styles/darkTheme";
 
 export function App () {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Header />
-      <Main />
+      <KanbanContextProvider>
+        <Header />
+        <Main />
+      </KanbanContextProvider>
     </ThemeProvider>
   )
 }
