c737d8c78658b35d2efa9e2fb3af3b137c2374c8
thiagocontelli <thiagocontellid@gmail.com>
2023-02-16 09:26:02 -0300

    feat: create the field to insert the card name and use the addList method

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
 import AddIcon from '@mui/icons-material/Add';
-import { Box, Button, Divider, Typography } from "@mui/material";
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  TextField,
+  Typography
+} from '@mui/material';
+import { useState } from 'react';
+import { useKanban } from '../../hooks/useKanban';
 
-export function Header () {
+export function Header() {
+  const h = useKanban();
+  const [isInputActive, setIsInputActive] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <>
       <Box
@@ -9,27 +24,60 @@ export function Header () {
           padding: '2rem 2rem 1rem 2rem',
           display: 'flex',
           alignItems: 'center',
-          justifyContent: 'space-between'
+          justifyContent: 'space-between',
+          height: '100px',
         }}
       >
         <Typography
           sx={{
             fontSize: '2rem',
             fontWeight: 'bold',
-            fontFamily: 'Poppins'
+            fontFamily: 'Poppins',
           }}
           variant="h1"
         >
           TC Kanban Board
         </Typography>
-        <Button 
-          variant="contained"
-          startIcon={<AddIcon />}
-        >
-          Add another list
-        </Button>
+        {isInputActive ? (
+          <Box display="flex" gap="0.5rem" alignItems="center">
+            <TextField
+              variant="filled"
+              size="small"
+              label="Card title"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <IconButton
+              onClick={() => {
+                h.addList(inputValue);
+                setIsInputActive(false);
+                setInputValue('');
+              }}
+            >
+              <CheckIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                setIsInputActive(false);
+                setInputValue('');
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        ) : (
+          <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            onClick={() => {
+              setIsInputActive(true);
+            }}
+          >
+            Add another list
+          </Button>
+        )}
       </Box>
       <Divider />
     </>
-  )
+  );
 }
